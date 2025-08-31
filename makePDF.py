721371d8fb55b27e75c494b244cfacd493ff6fbb
Cody Konior <cody@codykonior.com>
2025-08-31 14:12:36 +0800

    Improve with img2pdf

--- makePDF.py
+++ makePDF.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import os
 from PIL import Image
 from tqdm import tqdm
+import img2pdf
 
 class createBook:
 
@@ -12,14 +13,25 @@ class createBook:
         self.imageNameList = [os.path.join(path,'Images',x) for x in nameList]
 
     def makePdf(self):
-        firstPath = self.imageNameList[0]
-        width, height = Image.open(firstPath).size
-        pdf = FPDF(unit="pt", format=[width, height])
-        for pagePath in tqdm(self.imageNameList):
-            pdf.add_page()
-            pdf.image(pagePath,0,0)
-        if not os.path.exists(os.path.join(self.path,'Output')):
-            os.mkdir(os.path.join(self.path,'Output'))
-        name = str(self.name[:min(10,len(self.name))]).replace(" ","")
-        name = ''.join(ch for ch in name if ch.isalnum()) + ".pdf"
-        pdf.output(os.path.join(self.path,'Output',name),"F")
+        useimg2pdf = True
+
+        if useimg2pdf:
+            if not os.path.exists(os.path.join(self.path,'Output')):
+                os.mkdir(os.path.join(self.path,'Output'))
+            name = str(self.name[:min(10,len(self.name))]).replace(" ","")
+            name = ''.join(ch for ch in name if ch.isalnum()) + "_img2pdf.pdf"
+            img2pdf.default_dpi = 72.0
+            with open(os.path.join(self.path,'Output',name),"wb") as f:
+                f.write(img2pdf.convert(self.imageNameList))
+        else:
+            firstPath = self.imageNameList[0]
+            width, height = Image.open(firstPath).size
+            pdf = FPDF(unit="pt", format=[width, height])
+            for pagePath in tqdm(self.imageNameList):
+                pdf.add_page()
+                pdf.image(pagePath,0,0)
+            if not os.path.exists(os.path.join(self.path,'Output')):
+                os.mkdir(os.path.join(self.path,'Output'))
+            name = str(self.name[:min(10,len(self.name))]).replace(" ","")
+            name = ''.join(ch for ch in name if ch.isalnum()) + ".pdf"
+            pdf.output(os.path.join(self.path,'Output',name),"F")
